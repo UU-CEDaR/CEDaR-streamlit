c657398ad9dd5e50865f7c9a7d271de3e786c45d
nathandriggs54 <u0632236@umail.utah.edu>
2022-11-15 13:41:31 -0700

    Added comments to the code and a word document explaining the variables on the config file/

--- classes/VisualizationCreator.py
+++ classes/VisualizationCreator.py
@@ -130,8 +130,22 @@ class VisualizationCreator:
 
 #########################################################################################
 
-	def load_child(self, pollutant, year, filename):
-		self.image, self.min, self.max, self.caption = self.load_data_child(pollutant, year, filename)
+
+	def load_child(self, pollutant, year, sourceFileInfo):
+		"""
+		Gets the map information from the visualization
+		Loads a layer onto the base map for the visualization
+
+		Args:
+			pollutant: The pollutant that was selected
+			year: The year that was selected
+			sourceFileInfo: The information on the source file, saved as a dict.
+
+		Returns:
+			None
+			
+		"""
+		self.image, self.min, self.max, self.caption = self.load_data_child(pollutant, year, sourceFileInfo)
 		self.foliumMapValues = self.dataset["folium_map"]
 		m = folium.Map(location = [self.foliumMapValues["latitude"], self.foliumMapValues["longitude"]],
 					min_zoom=self.foliumMapValues["min_zoom"], max_zoom=self.foliumMapValues["max_zoom"],
@@ -149,19 +163,43 @@ class VisualizationCreator:
 
 		
 	@st.cache
-	def load_data_child(self, comp, year, sourceFile):
+	def load_data_child(self, pollutant, year, sourceFile):
+		"""
+		Chooses a way to load the child based on the file type.
+
+		Args:
+			pollutant: The pollutant that was selected
+			year: The year that was selected
+			sourceFileInfo: The information on the source file, saved as a dict.
+
+		Returns:
+			None
+			
+		"""
 		if (sourceFile["fileType"] == "zarr"):
-			return self.load_zarr_child(comp, year, sourceFile["fileName"])
+			return self.load_zarr_child(pollutant, year, sourceFile["fileName"])
 		if (sourceFile["fileType"] == "shp"):
-			return self.load_shp_child(comp, year, sourceFile["fileName"])
+			return self.load_shp_child(pollutant, year, sourceFile["fileName"])
 		else: #Do more later.
 			return None
 
 	@st.cache
-	def load_zarr_child(self, comp, year, filename):
-		ds = xr.open_zarr(filename) #how to get file name?
-		caption = f'{ds[comp].standard_name} ({ds[comp].units})'
-		data = ds[comp].loc[f"{year}-01-01"].data
+	def load_zarr_child(self, pollutant, year, sourceFileInfo):
+		"""
+		Loads a zarr to be used as a child.
+
+		Args:
+			pollutant: The pollutant that was selected
+			year: The year that was selected
+			sourceFileInfo: The information on the source file, saved as a dict.
+
+		Returns:
+			A color map, the min of the data, the max of the data, and the caption.
+			
+		"""
+		ds = xr.open_zarr(sourceFileInfo) #how to get file name?
+		caption = f'{ds[pollutant].standard_name} ({ds[pollutant].units})'
+		data = ds[pollutant].loc[f"{year}-01-01"].data
 		min, max = data.min(), data.max()
 		data = (data - data.min()) / (data.max() - data.min()) # normalize
 		cm = plt.cm.get_cmap('viridis_r')
@@ -175,8 +213,21 @@ class VisualizationCreator:
 ###########################################################################################
 	
 	@st.cache
-	def load_chloropleth(self, gdfFilename, dfFilename, chloropleth, comp, year):
-		df, gdf = self.load_shp_child(gdfFilename, dfFilename, comp, year)
+	def load_chloropleth(self, gdfFilename, dfFilename, pollutant, year):
+		"""
+		Loads a zarr to be used as a child.
+
+		Args:
+			gdfFilename: The name of the gdf to be loaded. See caces.py
+			dfFilename: The name of the df to be loaded. See caces.py
+			pollutant: The pollutant that was selected
+			year: The year that was selected
+
+		Returns:
+			A color map, the min of the data, the max of the data, and the caption.
+			
+		"""
+		df, gdf = self.load_shp_child(gdfFilename, dfFilename, pollutant, year)
 		m = folium.Map(location=[39.6, -111.5],
                    min_zoom=6, max_zoom=12, zoom_start=7)
 		# folium.GeoJson(data=gdf["geometry"]).add_to(m)
@@ -189,15 +240,28 @@ class VisualizationCreator:
 			nan_fill_color='red',
 			fill_opacity=0.7,
 			line_opacity=0.2,
-			legend_name=CAPTION[comp],
+			legend_name=CAPTION[pollutant],
 		).add_to(m)
 		# call to render Folium map in Streamlit
 		folium_static(m, width=700, height=800)
 
 	@st.cache
-	def load_shp_child(self, gdfFilename, dfFilename, year, comp):
+	def load_shp_child(self, gdfFilename, dfFilename, year, pollutant):
+		"""
+		Reads the file from the gdf
+
+		Args:
+			gdfFilename: The name of the gdf to be loaded. See caces.py
+			dfFilename: The name of the df to be loaded. See caces.py
+			year: The year that was selected
+			pollutant: The pollutant that was selected
+
+		Returns:
+			A color map, the min of the data, the max of the data, and the caption.
+			
+		"""
 		gdf = geopandas.read_file(gdfFilename, dfFilename)
 		gdf = gdf[['GEOID10','NAMELSAD10','geometry']]
 		df = pd.read_csv(dfFilename)
-		df = df[(df.year==year) & (df.pollutant==comp)][['fips','pred_wght']].astype({'fips':'str'})	
+		df = df[(df.year==year) & (df.pollutant==pollutant)][['fips','pred_wght']].astype({'fips':'str'})	
 		
